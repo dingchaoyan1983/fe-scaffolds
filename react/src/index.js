e93ddf2b112ba259c2ea061ab79dd11b817278b7
Ding.Chao <chao.ding@mail.dealmoon.com>
2016-10-03 22:36:23 +0800

    add react scaffolds

---
+++ react/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'css/scss/index.scss';
+
+ReactDOM.render(
+  <div>react scaffolds</div>, document.getElementById('react-root'))
