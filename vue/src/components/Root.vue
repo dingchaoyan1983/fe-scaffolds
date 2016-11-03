8960d0b7e182178bb0b247bf9601678260d3e328
Ding.Chao <chao.ding@mail.dealmoon.com>
2016-11-03 14:12:39 +0800

    use transform-vue-jsx plugin

--- vue/src/components/Root.vue
+++ vue/src/components/Root.vue
@@ -6,6 +6,38 @@
 
 <script>
   export default {
-    props: ['message']
+    props: ['message'],
+    beforeCreate() {
+      console.log(this._uid);
+      console.log('beforeCreate');
+    },
+    created() {
+      console.log(this._uid);
+      console.log('created');
+    },
+    beforeMount() {
+      console.log(this._uid);
+      console.log('beforeMount');
+    },
+    mounted() {
+      console.log(this._uid);
+      console.log('mounted');
+    },
+    beforeUpdate() {
+      console.log(this._uid);
+      console.log('beforeUpdate');
+    },
+    updated() {
+      console.log(this._uid);
+      console.log('updated');
+    },
+    beforeDestroy() {
+      console.log(this._uid);
+      console.log('beforeDestroy');
+    },
+    destroyed() {
+      console.log(this._uid);
+      console.log('destroyed');
+    }
   }
 </script>
--- vue/src/index.js
+++ vue/src/index.js
@@ -6,15 +6,13 @@ window.vm = new Vue({
   el: '#vue-root',
   data(){
     return {
-      msg: '我是msg'
+      msg: '我是msg',
+      switch: true
     }
   },
-  render(createElement) {
-    return createElement(Root, {
-      props: {
-        message: this.msg
-      }
-    })
-
+  render(h) {
+    return <div>
+              {this.switch ? <Root message={this.msg}/> : <div>这个是switch</div>}
+           </div>;
   }
 });
