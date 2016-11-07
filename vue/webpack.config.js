4d45e8ee715b79e428af41ca652049a69b1dc85b
Ding.Chao <chao.ding@mail.dealmoon.com>
2016-11-07 11:49:37 +0800

    add webpack-bundle-analyzer

--- vue/webpack.config.js
+++ vue/webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 var path = require('path');
 var autoprefixer = require('autoprefixer');
 var src = 'src';
@@ -85,7 +86,26 @@ var config = {
     }),
     new webpack.HotModuleReplacementPlugin(),
     new ExtractTextPlugin('[name].[hash].css'),
-    new webpack.BannerPlugin('This file is created by dding')
+    new webpack.BannerPlugin('This file is created by dding'),
+    new BundleAnalyzerPlugin({
+      // Can be `server`, `static` or `disabled`.
+      // In `server` mode analyzer will start HTTP server to show bundle report.
+      // In `static` mode single HTML file with bundle report will be generated.
+      // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting `generateStatsFile` to `true`.
+      analyzerMode: 'server',
+      // Port that will be used by in `server` mode to start HTTP server.
+      analyzerPort: 8888,
+      // Path to bundle report file that will be generated in `static` mode.
+      // If relative path is provided, it will be relative to bundles output directory
+      reportFilename: 'report.html',
+      // Automatically open report in default browser
+      openAnalyzer: true,
+      // If `true`, Webpack Stats JSON file will be generated in bundles output directory
+      generateStatsFile: false,
+      // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
+      // Relative to bundles output directory.
+      statsFilename: 'stats.json',
+    })
   ],
   devServer: {
     inline: true,
